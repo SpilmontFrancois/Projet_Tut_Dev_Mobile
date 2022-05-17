750084325277ed3dc6244f1f8c9d2d5810a9991d
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2022-05-17 19:25:01 +0200

    feat(nav): add logout

--- vuejs/components/NavBar.vue
+++ vuejs/components/NavBar.vue
@@ -9,14 +9,20 @@
     <router-link class="m-0" to="/profile">
       <fa-icon :icon="['fas', 'user-large']" color="white" />
     </router-link>
-    <router-link class="m-0" to="/messages">
-      <fa-icon :icon="['fas', 'message']" color="white" />
-    </router-link>
+    <div class="m-0" @click="logout">
+      <fa-icon :icon="['fas', 'sign-out-alt']" color="white" />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'NavBarComponent',
+  methods: {
+    logout() {
+      this.$auth.logout()
+      this.$router.push('/')
+    },
+  },
 }
 </script>
