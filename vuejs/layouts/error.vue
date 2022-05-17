27ca56f372c018661eab478a58801c34386adcc8
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2022-05-17 20:28:56 +0200

    feat(app): add error layout

---
+++ vuejs/layouts/error.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="center-component text-xl space-y-10 card p-10 rounded-card w-2/3">
+    <h1 class="text-dark-purple flex justify-center">
+      <strong>Erreur</strong>
+    </h1>
+    <div class="border-b border-gray" />
+    <h1 v-if="error.statusCode === 404" class="flex justify-center">
+      Page non trouvée
+    </h1>
+    <h1 v-else class="flex justify-center">Une erreur est survenue</h1>
+    <NuxtLink
+      :to="'/'"
+      class="flex justify-center bg-dark-purple text-back rounded-full p-2"
+    >
+      Retour à l'accueil
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorLayout',
+  layout: 'auth',
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
