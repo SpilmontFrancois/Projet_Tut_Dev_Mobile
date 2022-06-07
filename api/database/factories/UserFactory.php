f6d0b2ee8a10c48528c2c11c91b711368b6b5d15
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2022-06-07 18:59:18 +0200

    fix(factory): fix

--- api/database/factories/UserFactory.php
+++ api/database/factories/UserFactory.php
@@ -23,7 +23,7 @@ class UserFactory extends Factory
             'lastname' => $this->faker->lastName(),
             'email' => $this->faker->email(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'avatar' => 'https://picsum.photos/200?image='.rand(1, 1000),
+            'avatar' => 'https://picsum.photos/200?image='.rand(1, 705),
             'bio' => $this->faker->sentence(),
         ];
     }
