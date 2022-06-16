99ec787fecbe668e83e4993318bc1fb5a252878f
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2022-06-16 21:25:09 +0200

    fix(icons): fix icons maybe

--- vuejs/components/NavBar.vue
+++ vuejs/components/NavBar.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="bg-purple flex justify-around items-center h-14">
-    <NuxtLink class="m-0" to="/feed">
-      <fa-icon :icon="['fas', 'home']" color="white" class="w-5 h-5" />
+    <NuxtLink to="/feed">
+      <fa-icon :icon="['fas', 'house']" color="white" class="w-5 h-5" />
     </NuxtLink>
-    <NuxtLink class="m-0" to="/profile">
+    <NuxtLink to="/profile">
       <fa-icon :icon="['fas', 'user-large']" color="white" class="w-5 h-5" />
     </NuxtLink>
-    <div class="m-0" @click="logout">
-      <fa-icon :icon="['fas', 'sign-out-alt']" color="white" class="w-5 h-5" />
+    <div @click="logout">
+      <fa-icon
+        :icon="['fas', 'right-from-bracket']"
+        color="white"
+        class="w-5 h-5"
+      />
     </div>
   </div>
 </template>
